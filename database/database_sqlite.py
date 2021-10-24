be9aa829f9c5690b2bf0d5e7f97099b5bbd85a54
Aleksandr Khorev <sasha.horev.1@gmail.com>
2021-10-24 23:07:40 +0300

    Updated finder. Fixed database_sqlite.

--- database/database_sqlite.py
+++ database/database_sqlite.py
@@ -11,37 +11,45 @@ from typing import Tuple
 class ImageDatabase:
 
     def __init__(self, database_path):
+        
         self.db_path = database_path
 
     # Filling functions
 
     def add_new_image(self, photo_name: str):
+        
         current_dt = datetime.utcnow()
         current_dt = str(current_dt).split('.')[0]
+        
         self._open_database()
         result = self.cursor.execute("INSERT INTO Photos (PhotoName, UploadDateTime) VALUES (?, ?)",
                                      (photo_name, current_dt))
         entry_id = result.lastrowid
         self._close_database()
+        
         return entry_id
 
     def add_datetime(self, entry_id: int, date_time: datetime):
+        
         self._open_database()
         self.cursor.execute("UPDATE Photos SET PhotoDateTime=? WHERE PhotoID=?", (date_time, entry_id))
         self._close_database()
 
     def add_location(self, entry_id: int, latitude: float, longitude: float):
+        
         self._open_database()
         self.cursor.execute("UPDATE Photos SET LocationLatitude=?, LocationLongitude=? WHERE PhotoID=?",
                             (latitude, longitude, entry_id))
         self._close_database()
 
     def add_weather(self, entry_id: int, weather: int):
+        
         self._open_database()
         self.cursor.execute("UPDATE Photos SET Weather=? WHERE PhotoID=?", (weather, entry_id))
         self._close_database()
 
     def add_color(self, entry_id: int, red_value: float, green_value: float, blue_value: float, color_percentage: int):
+        
         self._open_database()
         self.cursor.execute(
             "INSERT INTO Colors (PhotoID, ColorRed, ColorGreen, ColorBlue, ColorPercentage) VALUES (?, ?, ?, ?, ?)",
@@ -51,19 +59,23 @@ class ImageDatabase:
     # Request functions
 
     def get_all_image_names(self):
+        
         self._open_database()
         result = self.cursor.execute("SELECT PhotoName FROM Photos")
         result = result.fetchall()
         self._close_database()
+        
         return result
 
     def get_image_name_by_exact_color(self, red_value: float, green_value: float, blue_value: float):
+        
         self._open_database()
         result = self.cursor.execute(
             "SELECT PhotoName FROM Photos WHERE PhotoID IN (SELECT PhotoID FROM Colors WHERE ColorRed=? AND ColorGreen=? AND ColorBlue=?)",
             (red_value, green_value, blue_value))
         result = result.fetchall()
         self._close_database()
+        
         return result
 
     def get_entry_id_by_parameters(self, hour_interval_left: str, hour_interval_right: str,
@@ -91,15 +103,18 @@ class ImageDatabase:
         print(request_text)
         
         self._open_database()
-        
         result = self.cursor.execute(request_text)
-
         result = result.fetchall()
-        
         self._close_database()
-        return result
+        
+        result_list = []
+        for row in result:
+            result_list.append(row[0])
+        
+        return result_list
 
     def get_image_name_by_entry_id(self, entry_id: int):
+        
         self._open_database()
         result = self.cursor.execute("SELECT PhotoName FROM Photos WHERE PhotoID=?", (entry_id,))
         result = result.fetchall()
@@ -109,6 +124,7 @@ class ImageDatabase:
             return row[0]
 
     def get_location_by_entry_id(self, entry_id: int) -> Tuple[float, float]:
+        
         self._open_database()
         result = self.cursor.execute("SELECT LocationLatitude, LocationLongitude FROM Photos WHERE PhotoID=?",
                                      (entry_id,))
@@ -119,30 +135,38 @@ class ImageDatabase:
             return row[0], row[1]
 
     def get_colors_by_entry_id(self, entry_id: int):
+        
         self._open_database()
-        result = self.cursor.execute("SELECT ColorRed, ColorGreen, ColorBlue FROM Colors WHERE PhotoID=?", (entry_id,))
+        result = self.cursor.execute("SELECT ColorRed, ColorGreen, ColorBlue, ColorPercentage FROM Colors WHERE PhotoID=?", (entry_id,))
         result = result.fetchall()
         self._close_database()
-        return result
+        
+        for row in result:
+            return row[0], row[1], row[2], row[3]
 
     # Service functions
 
     def _open_database(self):
+        
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
 
     def _close_database(self):
+        
         self.connection.commit()
         self.connection.close()
 
     def _delete_entry(self, entry_id: int):
+        
         self._open_database()
         self.cursor.execute("DELETE FROM Photos WHERE PhotoID=?", (entry_id,))
         self._close_database()
 
     def _get_all_entries(self, table_name: str):
+        
         self._open_database()
         self.cursor.execute("SELECT * FROM {}".format(table_name))
         entries = self.cursor.fetchall()
         self._close_database()
+        
         return entries
